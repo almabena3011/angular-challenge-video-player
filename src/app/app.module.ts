16ecb8243e9ea15ad664de6effc14025b62d7a20
almabena3011 <abestnainggolan@gmail.com>
2024-03-17 22:12:49 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { VideoPlayerTestComponent } from './component/video-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent, VideoPlayerTestComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/video-player.component.ts
@@ -0,0 +1,54 @@
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import videojs from 'video.js';
+
+@Component({
+    selector: "app-video-player",
+    templateUrl: "./video-player.component.html",
+    styleUrls: [
+        "./video-player.component.css",
+    ]
+})
+export class VideoPlayerTestComponent implements OnInit, OnDestroy {
+  player: any;
+  @Input() videoLink: string = '';
+  isMuted: boolean = false;
+  muteLabel: string = "Mute";
+  constructor() { }
+
+  ngOnInit(): void {
+    this.player = videojs('video-source');
+  }
+
+  playVideo(): void {
+    this.player.play();
+  }
+
+  restartVideo(): void {
+    this.player.currentTime(0);
+    this.playVideo();
+  }
+
+  pauseVideo(): void {
+    this.player.pause();
+  }
+
+  muteVideo(): void {
+    if(!this.isMuted){
+      this.isMuted = true;
+      this.muteLabel = 'Unmute';
+      this.player.muted(true);
+    } else{
+      this.isMuted = false;
+      this.muteLabel = 'Mute';
+      this.player.muted(false);
+    }
+  }
+
+   // Dispose the player OnDestroy
+   ngOnDestroy() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+
+}
